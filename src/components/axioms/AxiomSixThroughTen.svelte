fa2d66118f78c7dbbfc66b5d46f5aae73edf16ac
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-10-29 21:41:00 +0000

    Update AxiomSixThroughTen.svelte

--- src/components/axioms/AxiomSixThroughTen.svelte
+++ src/components/axioms/AxiomSixThroughTen.svelte
@@ -4,47 +4,24 @@
 
 <Typewriter cascade interval={20}>
   <br />
-  <ol>
-    <li>
-      <p>6. <span class="text-delta-green">Other than sticking to your cover, never lie to a fellow member of Delta Green.</span></p>
-      <br />
-      <p class="pl-6">  About anything. </p>
-      <br />
-      <p class="pl-6">  <span class="text-delta-green">Ever.</span> </p>
-      <br />
-      <p class="pl-6">  Unless A-cell says otherwise. </p>
-    </li>
-    <br />
-    <li>
-      <p>7. <span class="text-delta-green">Always lie to anyone who isn't a member of Delta Green.</span></p>
-      <br />
-      <p class="pl-6">  About everything. </p>
-      <br />
-      <p class="pl-6">  <span class="text-delta-green">Always.</span> </p>
-      <br />
-      <p class="pl-6">  And make sure your lies are specific and consistent. </p>
-    </li>
-    <br />
-    <li>
-      <p>8. <span class="text-delta-green">Friendlies can know the truth about an individual operation.</span></p>
-      <br />
-      <p class="pl-6">  They can never know the <span class="text-delta-green">truth</span> about Delta Green. </p>
-    </li>
-    <br />
-    <li>
-      <p>9. <span class="text-delta-green">Always scout your destination, no matter how secure you think it is.</span></p>
-      <br />
-      <p class="pl-6">  Never bring the whole team to do surveillance. </p>
-      <br />
-      <p class="pl-6">  Be <span class="text-delta-green">inconspicuous.</span> </p>
-      <br />
-      <p class="pl-6">  See the enemy first. </p>
-    </li>
-    <br />
-    <li>
-      <p>10. <span class="text-delta-green">Always perform a counter-surveillance check before you camp down and before you move.</span></p>
-      <br />
-      <p class="pl-6">  Perfect <span class="text-delta-green">paranoia</span> is perfect awareness. </p>
-    </li>
-  </ol>
+<p class=MsoNormal><span class="text-delta-green">Jonathan
+Willet</span>- conferences – some articles he worked on no person of interests.</p>
+
+<p class=MsoNormal>Back door with a tag on it saying 046 -house key</p>
+
+<p class=MsoNormal>Tire screech and doors open</p>
+
+<p class=MsoNormal>Security company- Ghast -licence plate of WJ18 LXP</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Broadmoor strongly rebuffing any attempts to see the <span
+class=GramE>doctor</span> </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>A lot of property for her to have- research Lucy Hawthorne-
+financial records- Land registry- Family- nothing significant home counties,
+middle class, parent divorced, mum moved abroad, Dad died from heart attack (Mr.
+_________ Hawthorn)</p>
 </Typewriter>
