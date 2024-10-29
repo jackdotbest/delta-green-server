8b8019c61b86b16601f1952adca7fa9291bd49a0
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-10-29 19:31:12 +0000

    Update AxiomOneThroughFive.svelte

--- src/components/axioms/AxiomOneThroughFive.svelte
+++ src/components/axioms/AxiomOneThroughFive.svelte
@@ -6,21 +6,115 @@
   <br />
   <ol>
     <li>
-      <p class=MsoNormal><span style='background:yellow;mso-highlight:yellow'>Charles Dexter Ward (17)</span> -still missing- no body been found </p>
+<p class=MsoNormal><span class="text-delta-green">Charles
+Dexter Ward (17)</span> -still missing- no body been found </p>
 
-      <p class=MsoNormal>Found out address of the childhood house </p>
+<p class=MsoNormal>Found out address of the childhood house </p>
 
-      <p class=MsoNormal>In care of Doctor Johnathan Willet -Broadmoor highs security psychiatric hospital in Crowthorne, Berkshire<br style='mso-special-character:line-break'>
-      <p class=MsoNormal>Feels his mother lied<br>
-      Has delusions of grandeur- based on a supposed birth certificate of his
-      grandfather was not the name on the birth certificate</p>
+<p class=MsoNormal>In care of Doctor Johnathan Willet -Broadmoor highs security
+psychiatric hospital in Crowthorne, Berkshire<br style='mso-special-character:
+line-break'>
 
-      <p class=MsoNormal>What’s the name <span style='background:yellow;mso-highlight:
-      yellow'>Joseph Curwin</span></p>
+<p class=MsoNormal>Feels his mother lied<br>
+Has delusions of grandeur- based on a supposed birth certificate of his
+grandfather was not the name on the birth certificate</p>
 
-      <p class=MsoNormal>Was in therapy since 13 </p>
+<p class=MsoNormal>What’s the name <span style='background:yellow;mso-highlight:
+yellow'>Joseph Curwin</span></p>
 
-      <p class=MsoNormal>Providence Rhode Island </p>
+<p class=MsoNormal>Was in therapy since 13 </p>
+
+<p class=MsoNormal>Providence Rhode Island </p>
+
+<p class=MsoNormal><span class="text-delta-green">Charles</span>-
+reported interview with school principal- <span style='background:yellow;
+mso-highlight:yellow'>Claire Rushmore</span>= found a contact number <br>
+- name redacted of school </p>
+
+<p class=MsoNormal>30 Sheldon Avenue- light green door- Toyota out-front </p>
+
+<p class=MsoNormal>They have CCTV cameras</p>
+
+<p class=MsoNormal>-Year 2018 happened 10pm evening May 31<sup>st</sup> 2018</p>
+
+<p class=MsoNormal><span class="text-delta-green">Dr
+willet-</span> got in house, stabbed <span style='background:yellow;mso-highlight:
+yellow'>Lucy Hawthorn</span>- Stabbed a number of times with a kitchen knife</p>
+
+<p class=MsoNormal>Journalist focused on videogames <br>
+Back catalog suggests more fantasy <br>
+<br>
+Lived alone- HOW DID SHE AFFORD THE HOUSE</p>
+
+<p class=MsoNormal>Multiple souls in the house- Maybe people have died and are
+lingering </p>
+
+<p class=MsoNormal>Not far from the grave of <span style='background:yellow;
+mso-highlight:yellow'>Karl Marx</span> </p>
+
+<p class=MsoNormal>No damage to the property nor at the back- all happened
+inside the house</p>
+
+<p class=MsoNormal>Too fresh- construction, noise, banners, scaffolding a few
+doors down.</p>
+
+<p class=MsoNormal>House- no one currently living there- no for sale sign- mail
+on the floor but lawn maintained, back garden well maintained- all curtains
+drawn</p>
+
+<p class=MsoNormal>Nothing relevant about the house- no burial sites BUT A
+FUCKING GRAVEYARD nearby and a crematorium.</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal><span class="text-delta-green">Jonathan
+Willet</span>- conferences – some articles he worked on no person of interests.</p>
+
+<p class=MsoNormal>Back door with a tag on it saying 046 -house key</p>
+
+<p class=MsoNormal>Tire screech and doors open</p>
+
+<p class=MsoNormal>Security company- Ghast -licence plate of WJ18 LXP</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Broadmoor strongly rebuffing any attempts to see the <span
+class=GramE>doctor</span> </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>A lot of property for her to have- research Lucy Hawthorne-
+financial records- Land registry- Family- nothing significant home counties,
+middle class, parent divorced, mum moved abroad, Dad died from heart attack (Mr.
+_________ Hawthorn)</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Lucy Hawthorne- 28 years young </p>
+
+<p class=MsoNormal>GRAND PARENTS- mother side Tillinghast </p>
+
+<p class=MsoNormal>Godfrey Tillinghast- Cabinet secretary 2008- 2016- head of
+civil service- one of the most powerful positions in the country- in charge of
+the people in the country </p>
+
+<p class=MsoNormal>Wife called Caroline Tillinghast- there daughter Emma
+Tillinghast is Lucy’s mother- </p>
+
+<p class=MsoNormal>Few months already in Broadmoor seems fast </p>
+
+<p class=MsoNormal>GHAST security seems to link to Tillinghast, however not
+listed in the name of directors </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Note- we have a contact who might be able to get us to talk
+to Doctor Johnathan Willet </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Rumours of a connection between Lucy Hawthorn and Doctor
+Johnathan Willet from Broadmoor contact.</p>
     </li>
   </ol>
 </Typewriter>
