ebef94ea3ef519bb58151e77f003a5e9da3b3e41
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-12 18:46:11 +0000

    Update JonathanWillet.svelte

--- src/components/axioms/JonathanWillet.svelte
+++ src/components/axioms/JonathanWillet.svelte
@@ -1,1 +1,15 @@
+<script>
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <h1><span class="text-delta-green">Dr. Jonathan Willet</span></h1>
+  <p>- Former psychiatrist for Charles Dexter Ward.</p>
+  <p>- Currently incarcerated at Broadmoor High-Security Psychiatric Hospital.</p>
+  <p>- Arrested, sentenced, and admitted to Broadmoor within weeks of Lucy Hawthorne’s death.</p>
+  <p>- Broadmoor has denied all requests for access to Dr. Willet.</p>
+  <p>- An internal contact suggests exploring the victim’s family background for links to Dr. Willet’s rapid sentencing.</p>
+  <br />
+</Typewriter>
 
