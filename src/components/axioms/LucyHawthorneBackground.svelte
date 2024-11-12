244650c89be91337e3917fac8445691dc317d195
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-12 18:47:31 +0000

    Create LucyHawthorneBackground.svelte

---
+++ src/components/axioms/LucyHawthorneBackground.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <h1><span class="text-delta-green">Lucy Hawthorne's Background</span></h1>
+  <p>- Freelance journalist, specializing in video games, with a focus on fantasy themes.</p>
+  <p>- Financial situation suggests potential sources of income beyond journalism.</p>
+  <p>- Family Background:</p>
+  <p>  - Parents divorced; mother lives abroad, and father passed away from a heart attack.</p>
+  <p>  - Maternal grandparents: <span class="text-delta-green">Godfrey and Caroline Tillinghast</span>.</p>
+  <p>  - Godfrey Tillinghast was Cabinet Secretary (2008–2016), a role of significant influence.</p>
+  <p>  - <span class="text-delta-green">Ghast Security</span> possibly linked to the Tillinghast family.</p>
+  <br />
+</Typewriter>
