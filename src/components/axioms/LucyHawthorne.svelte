26a4eed31f494566d99e7148033bb8d08460151f
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-12 18:46:49 +0000

    Create LucyHawthorne.svelte

---
+++ src/components/axioms/LucyHawthorne.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <h1><span class="text-delta-green">Lucy Hawthorne (28)</span></h1>
+  <p>- Lived at 30 Sheldon Avenue, with a light green door and a Toyota out front.</p>
+  <p>- Fatal incident occurred on May 31, 2018, around 10:00 PM.</p>
+  <p>- Dr. Willet allegedly entered her home and stabbed her multiple times with a kitchen knife.</p>
+  <p>- Lived alone; property is well-maintained despite her passing.</p>
+  <p>- Nearby graveyard and crematorium, though no burial sites on the property.</p>
+  <p>- Discovered a “fake stone” by the back door with a key labeled "046".</p>
+  <p>- <span class="text-delta-green">Ghast Security</span> responded five minutes after investigators entered.</p>
+  <br />
+</Typewriter>
