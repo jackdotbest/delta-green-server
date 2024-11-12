89b0ab847873dda78b678fcc38bc7e38c9dcbe07
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-12 17:20:16 +0000

    Update AxiomSixThroughTen.svelte

--- src/components/axioms/AxiomSixThroughTen.svelte
+++ src/components/axioms/AxiomSixThroughTen.svelte
@@ -4,24 +4,20 @@
 
 <Typewriter cascade interval={20}>
   <br />
-<p class=MsoNormal><span class="text-delta-green">Jonathan
-Willet</span>- conferences – some articles he worked on no person of interests.</p>
+<p class="MsoNormal"><span class="text-delta-green">Lucy Hawthorn</span></p>
+    <p class="MsoNormal">Lived alone; unclear how she could afford the house.</p>
+    <p class="MsoNormal">Multiple souls may be lingering in the house, possibly due to past deaths.</p>
+    <p class="MsoNormal">House located near a graveyard and crematorium.</p>
+    <p class="MsoNormal">No current residents, no sale sign, but mail is piled on the floor, and the lawn/back garden is well-maintained; all curtains drawn.</p>
+    <p class="MsoNormal">No significant damage to property, but construction noise nearby.</p>
+    <p class="MsoNormal">Family Background:</p>
+    <ul class="MsoNormal">
+      <li class="MsoNormal">Parents: Divorced; mother moved abroad, father (Mr. _______ Hawthorn) died of a heart attack.</li>
+      <li class="MsoNormal">Age: 28 years.</li>
+      <li class="MsoNormal">Maternal Grandparents: <span class="text-delta-green">Godfrey Tillinghast</span>, former Cabinet Secretary (2008-2016), head of civil service; wife <span class="text-delta-green">Caroline Tillinghast</span>; daughter <span class="text-delta-green">Emma Tillinghast</span> is Lucy’s mother.</li>
+    </ul>
+    <p class="MsoNormal">Last known event: May 31, 2018, at 10pm—Dr. Jonathan Willet entered the house and stabbed Lucy Hawthorn multiple times with a kitchen knife.</p>
+    <p class="MsoNormal">Address: 30 Sheldon Avenue, light green door, Toyota parked outside. CCTV cameras installed.</p>
 
-<p class=MsoNormal>Back door with a tag on it saying 046 -house key</p>
-
-<p class=MsoNormal>Tire screech and doors open</p>
-
-<p class=MsoNormal>Security company- Ghast -licence plate of WJ18 LXP</p>
-
-<p class=MsoNormal><o:p>&nbsp;</o:p></p>
-
-<p class=MsoNormal>Broadmoor strongly rebuffing any attempts to see the <span
-class=GramE>doctor</span> </p>
-
-<p class=MsoNormal><o:p>&nbsp;</o:p></p>
-
-<p class=MsoNormal>A lot of property for her to have- research Lucy Hawthorne-
-financial records- Land registry- Family- nothing significant home counties,
-middle class, parent divorced, mum moved abroad, Dad died from heart attack (Mr.
-_________ Hawthorn)</p>
+    <br />
 </Typewriter>
