4f77806d20052a4552d5252ace774366ac05afb0
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-12 18:22:24 +0000

    Update CharlesDexterWard.svelte

--- src/components/axioms/CharlesDexterWard.svelte
+++ src/components/axioms/CharlesDexterWard.svelte
@@ -5,9 +5,9 @@
 <Typewriter cascade interval={20}>
   <br />
   <h1><span class="text-delta-green">Charles Dexter Ward (17)</span></h1>
-  <p>- Still missing, no body found.</p>
-  <p>- Discovered childhood home address; currently in care of Dr. Jonathan Willet at Broadmoor High Security Psychiatric Hospital, Crowthorne, Berkshire.</p>
-  <p>- Believes his mother lied to him.</p>
+  <p>- Still missing, no body found, after disappearing while in a secure psychiatric facility under the care of Dr Willet</p>
+  <p>- Discovered childhood home address on Prospect Street, Providence, Rhode Island.</p>
+  <p>- Believes his mother lied to him about his ancestry.</p>
   <p>- Suffers from delusions of grandeur; claims his grandfather’s name on his birth certificate is incorrect.</p>
   <p>- Connection to <span class="text-delta-green">Joseph Curwin</span>?</p>
   <p>- In therapy since age 13, based in Providence, Rhode Island.</p>
