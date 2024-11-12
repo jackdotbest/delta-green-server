9c6a6d4fb35e65926de615860b874958e7fea97c
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-12 17:10:52 +0000

    Update AxiomOneThroughFive.svelte

--- src/components/axioms/AxiomOneThroughFive.svelte
+++ src/components/axioms/AxiomOneThroughFive.svelte
@@ -4,61 +4,15 @@
 
 <Typewriter cascade interval={20} >
   <br />
-<p class=MsoNormal><span class="text-delta-green">Charles
-Dexter Ward (17)</span> -still missing- no body been found </p>
+  <Typewriter cascade interval={20}>
+    <!-- Charles Dexter Ward Section -->
+    <br />
+    <p class="MsoNormal"><span class="text-delta-green">Charles Dexter Ward (17)</span> - still missing, no body found.</p>
+    <p class="MsoNormal">Discovered childhood home address; currently in care of Dr. Jonathan Willet at Broadmoor High Security Psychiatric Hospital, Crowthorne, Berkshire.</p>
+    <p class="MsoNormal">Believes his mother lied to him.</p>
+    <p class="MsoNormal">Suffers from delusions of grandeur; claims his grandfather’s name on his birth certificate is incorrect.</p>
+    <p class="MsoNormal">Connection to <span class="text-delta-green">Joseph Curwin</span>?</p>
+    <p class="MsoNormal">In therapy since age 13, based in Providence, Rhode Island.</p>
 
-<p class=MsoNormal>Found out address of the childhood house </p>
-
-<p class=MsoNormal>In care of Doctor Johnathan Willet -Broadmoor highs security
-psychiatric hospital in Crowthorne, Berkshire</p>
-
-<p class=MsoNormal>Feels his mother lied
-Has delusions of grandeur- based on a supposed birth certificate of his
-grandfather was not the name on the birth certificate</p>
-
-<p class=MsoNormal>What’s the name <span class="text-delta-green">Joseph Curwin</span></p>
-
-<p class=MsoNormal>Was in therapy since 13 </p>
-
-<p class=MsoNormal>Providence Rhode Island </p>
-
-<br />
-
-<p class=MsoNormal><span class="text-delta-green">Charles</span>-
-reported interview with school principal- <span class="text-delta-green">Claire Rushmore</span>= found a contact number <br>
-- name redacted of school </p>
-
-<p class=MsoNormal>30 Sheldon Avenue- light green door- Toyota out-front </p>
-
-<p class=MsoNormal>They have CCTV cameras</p>
-
-<p class=MsoNormal>-Year 2018 happened 10pm evening May 31<sup>st</sup> 2018</p>
-
-<br />
-
-<p class=MsoNormal><span class="text-delta-green">Dr
-willet-</span> got in house, stabbed <span class="text-delta-green">Lucy Hawthorn</span>- Stabbed a number of times with a kitchen knife</p>
-
-<p class=MsoNormal>Journalist focused on videogames <br>
-Back catalog suggests more fantasy <br>
-<br>
-Lived alone- HOW DID SHE AFFORD THE HOUSE</p>
-
-<p class=MsoNormal>Multiple souls in the house- Maybe people have died and are
-lingering </p>
-
-<p class=MsoNormal>Not far from the grave of <span class="text-delta-green">Karl Marx</span> </p>
-
-<p class=MsoNormal>No damage to the property nor at the back- all happened
-inside the house</p>
-
-<p class=MsoNormal>Too fresh- construction, noise, banners, scaffolding a few
-doors down.</p>
-
-<p class=MsoNormal>House- no one currently living there- no for sale sign- mail
-on the floor but lawn maintained, back garden well maintained- all curtains
-drawn</p>
-
-<p class=MsoNormal>Nothing relevant about the house- no burial sites BUT A
-FUCKING GRAVEYARD nearby and a crematorium.</p>
+    <br />
 </Typewriter>
