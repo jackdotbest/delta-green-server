bb0f9e9e793b3b4e9a2bcd846007f486ce899bba
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-26 20:50:43 +0000

    Update Agents.ts

--- src/Agents.ts
+++ src/Agents.ts
@@ -1,6 +1,7 @@
 export const Agents: Array<{user: string, password: string}> = [
   {user: 'I.Castillex', password: 'YouCantHide!'},
   {user: 'L.Dawson', password: 'ISeeYou$'},
-  {user: 'Y.Yerque', password: 'GreenHook'},
+  {user: 'Y.Yerque', password: 'WeHearAll$'},
+  {user: 'tester', password: 'tester'},
 ]
 export default Agents;
