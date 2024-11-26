056f9f42bcd454dd08f3eeb9602fc8f63fa910d9
jackdotbest <50680238+jackdotbest@users.noreply.github.com>
2024-11-26 20:40:02 +0000

    Update InterviewSnippet01.svelte

--- src/components/axioms/InterviewSnippet01.svelte
+++ src/components/axioms/InterviewSnippet01.svelte
@@ -1,5 +1,156 @@
 <script>
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <h1><span class="text-delta-green">Charles Dexter Ward, aged 17, Interview with Dr Willet </span></h1>
+
+  <br />
+</Typewriter>
+
+<script>
     import AudioPlayer from "../AudioPlayer.svelte";
 </script>
 
 <AudioPlayer src="/audio/InterviewSnippet01.mp3" />
+
+<table class="Transcript" style="width: 100%; margin-bottom: 10px; border-collapse: collapse; border-bottom: 1px solid lime; background-color: black;" data-index-number="1">
+  <tbody>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Jonathan Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        So what has prompted this sudden interest in your ancestry, Charles?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles Dexter Ward
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        I don’t think it’s sudden. Does it seem sudden?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        Well, perhaps it’s just new to me.
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        I want to know who I am.
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        In the sense of?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        In every sense. I don’t think I’m the person they think I am.
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        Who’s “they"?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        People. You. My family.
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        Your family don’t know who you are?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        My mother did. But she lied.
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        What did she lie about, Charles?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        [We hear the first instance of the audio artefact containing the location of Haumea.]
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        I was up in the attic and I found something. She had one of those old files, you know those things that stretch out?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        A concertina file?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        A birth certificate was in there. My grandfather, her father. That’s not the name on the birth certificate.
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Dr. Willett
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        What’s the name?
+      </td>
+    </tr>
+    <tr style="border-bottom: 1px lime dotted;">
+      <td style="background: #333; padding: 3px 3px; text-align: right; white-space: nowrap; color: lime;">
+        Charles
+      </td>
+      <td style="padding: 3px 3px; text-align: left; color: lime;">
+        Joseph Curwen.
+      </td>
+    </tr>
+  </tbody>
+</table>
